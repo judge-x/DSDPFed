293c562d473e98543bb0b420e37a6c37bfb1d069
judge-x <37145229+judge-x@users.noreply.github.com>
2022-10-09 10:29:08 +0800

    Update and rename net_temp.py to net_Temp.py

--- FedAvg-Shuffle-Sample/ShuffleDP-Fed/net_Temp.py
+++ FedAvg-Shuffle-Sample/ShuffleDP-Fed/net_Temp.py
@@ -3,9 +3,9 @@ from Models import *
 class tp_net(object):
     def __init__(self,model_name):
         if model_name == 'mnist_2nn':
-            self.net = Mnist_2NN()  # 初始化网络2NN
+            self.net = Mnist_2NN()  # Initialising the network 2NN
         elif model_name == 'mnist_cnn':
-            self.net = Mnist_CNN()  # 初始化网络CNN
+            self.net = Mnist_CNN()  # Initialising the network CNN
         elif model_name == 'cifar_cnn':
             self.net = Cifar_CNN(10)
         elif model_name == 'cifar_resnet':
